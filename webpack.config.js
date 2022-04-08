80b2045dd68c1c2cd7bf061ede7e06039c7b4050
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-04-08 18:03:49 -0300

    POC

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module:{
+        rules:[
+            {
+                test: /\.css$/,
+                loader: 'style-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'css-loader'
+            }
+        ]
+    }
+};
